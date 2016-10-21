edcf863a75a3e6d5f026b5657c17b287efdbaa0c
Le Duc Thien <timducle@yahoo.com>
2016-10-21 10:20:19 +0700

    Restrict viewing tickets to project scope

--- spec/controllers/tickets_controller_spec.rb
+++ spec/controllers/tickets_controller_spec.rb
@@ -1,5 +1,14 @@
 require 'spec_helper'
 
 describe TicketsController do
+  let!(:user) { FactoryGirl.create(:user)}
+  let!(:project) { FactoryGirl.create(:project)}
+  let!(:ticket) { FactoryGirl.create(:ticket, project: project)}
 
+  it 'cannot see tickets of a project without permission' do
+    sign_in user
+    get :show, id: ticket.id, project_id: project.id
+    expect(response).to redirect_to(root_path)
+    expect(flash[:alert]).to eql 'Project not exist'
+  end
 end
