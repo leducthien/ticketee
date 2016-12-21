15524ae5ec988c48a7d5f8d55c5108b31bc37a9e
Le Duc Thien <timducle@yahoo.com>
2016-12-22 01:36:06 +0700

    Add state transition to comments

--- app/helpers/tickets_helper.rb
+++ app/helpers/tickets_helper.rb
@@ -1,2 +1,16 @@
 module TicketsHelper
+  def state_for(comment)
+    content_tag(:div, {class: 'state'}, false) do
+      previous_state = comment.previous_state
+      if comment.state
+        if previous_state && comment.state != previous_state
+          "#{render previous_state} &rarr; #{render comment.state}"
+        else
+          render comment.state
+        end
+      else
+        ''
+      end
+    end
+  end
 end
